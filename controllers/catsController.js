88f7eb0e45f09b9a76f400effb219e7508f816f9
Tommie <tommiealcorn@gmail.com>
2019-04-20 14:05:53 -0600

    1

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -3,20 +3,32 @@ const router = express.Router();
 const Cat = require('../models/Cat');
 const User = require('../models/User');
 
+// This gets all the cats from the Database and puts them into
+// the Template.
 router.get('/', (req, res)=>{
+    //  find cats on the DB
     Cat.find({}, (err, catsFromTheDatabase)=>{
+        // show the found cats on the screen
         res.render('cats/index.ejs', {
+            // move cats from the DB to the screen
             catsOnTheTemplate: catsFromTheDatabase
         })
     })
 })
 
+// This function creates a new cat by calling new.ejs
 router.get('/new', (req, res)=>{
+    //  find a user
     User.find({}, (error, allUsers) => {
+        // if there's an eror
         if(error) {
+            // sned the error to the function
             res.send(error)
+            // if there is no error
         } else {
+            //  show the new cats
             res.render('cats/new.ejs', {
+                //  move all the users from the DB to the screen
                 usersOnTemplate: allUsers
             })
         }
@@ -24,22 +36,34 @@ router.get('/new', (req, res)=>{
 
 })
 
+// This gets a cat by ID and shows it by calling show.ejs.
 router.get('/:id', (req, res) => {
+    //  find a cat by ID
     Cat.findById(req.params.id, (err, catFromTheDatabase) => {
+      //  find one user for a cat  
       User.findOne({
         "cats": req.params.id
+        //  check for errors
       }, (err, user) => {
+          // shgow the cat
         res.render('cats/show.ejs', {
+            // move the cat from the DB to the screen
           catOnTheTemplate: catFromTheDatabase,
+          //  move users to the screen
           user: user
         });
       })
     })
    })
 
+
+// this Edits an already existing cat by calling edit.ejs
 router.get('/:id/edit', (req, res)=>{
+    //  find a cat by ID from the DB
     Cat.findById(req.params.id, (err, catFromTheDatabase)=>{
+        // find the user from the DB
         User.find({}, (err, usersFromTheDatabase)=>{
+            //  show cats and users on the screen
             res.render('cats/edit.ejs', {
                 catOnTheTemplate: catFromTheDatabase,
                 usersOnTemplate: usersFromTheDatabase
@@ -49,11 +73,15 @@ router.get('/:id/edit', (req, res)=>{
     })
 })
 
-
+//  This gives a cat to a user 
 router.post('/', (req, res)=>{
+    // show the requestor body
     console.log(req.body);
+// Pass the body of the new Cat form to the Create function
     Cat.create(req.body, (err, newlyCreatedCat)=>{
+        // Log the newly created Cat
         console.log(`Created a cat for user ${req.body.userId}`);
+        // Verify that the User exists
         User.findById(req.body.userId, function(err, userFound)
         {
             userFound.cats.push(newlyCreatedCat._id);
@@ -66,12 +94,20 @@ router.post('/', (req, res)=>{
     })
 })
 
+// This gets a cat by ID and gives it to a user user then removes it
+// from the original user.
 router.put('/:id', (req, res)=>{
+    // log the request body
     console.log(req.body);
+    // pass the cat ID to the function
     Cat.findByIdAndUpdate(req.params.id, req.body, {new: true},(err, updatedCat)=>{
+      // 
       User.findOne({'cats': req.params.id}, (err, foundUser) => {
+        // check to make sure the cat is going to a new user
         if(foundUser._id.toString() !== req.body.userId){
+          // remove the cat from the original user
           foundUser.cats.remove(req.params.id);
+          // 
           foundUser.save((err, savedFoundUser) => {
             User.findById(req.body.userId, (err, newUser) => {
               newUser.cats.push(updatedCat._id);
@@ -80,6 +116,8 @@ router.put('/:id', (req, res)=>{
               })
             })
           })
+        // If the cat is not going to a new user, redirect back to
+        // the Cats screen 
         } else {
           res.redirect('/cats/' + req.params.id)
         }
@@ -87,23 +125,37 @@ router.put('/:id', (req, res)=>{
     });
   });
 
+// Thisa route gets an existing cat by ID them deletes it and removes the cat
+// from its user.
 router.delete('/:id', (req, res)=>{
+    // Find the cat by ID and Delete
     Cat.findByIdAndDelete(req.params.id, (err, catFromTheDatabase)=>{
+        // Log the cat that was removed from the DB
         console.log(catFromTheDatabase);
+        // Redirect back to the Cat screen
         res.redirect('/cats');
     })
 })
 router.delete('/:id', (req, res)=>{
+    // Find the Cat by ID and Delete
     Cat.findByIdAndDelete(req.params.id, (err, catFromTheDatabase)=>{
+        // Log the cat that was Deleted from the DB
         console.log(catFromTheDatabase);
+        // Find One User that had this cat
         User.findOne({'cats': req.params.id}, (err, foundUser)=>{
+            // If there was an error, Console.log the error
             if(err){
                 console.log(err)
+            // Otherwise, Log the User
             }else{
                 console.log(foundUser);
+                // Remove the cat from the user
                 foundUser.cats.remove(req.params.id);
+                // Save the Updated user
                 foundUser.save((err, updatedUser)=>{
+                    // Log the Updated User
                     console.log(updatedUser);
+                    // Redirect back to the Cats screen.
                     res.redirect('/cats');  
                 })
             };
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,64 +3,98 @@ const router = express.Router();
 const User = require('../models/User');
 const Cat = require('../models/Cat');
 
+// This function gets a list of all the Users
 router.get('/', (req, res)=>{
+    //  find users in the database
     User.find({}, (err, usersFromTheDatabase)=>{
+        //  Show the users on the screen
         res.render('users/index.ejs', {
+            //  move the users from the DB to the template
             usersOnTheTemplate: usersFromTheDatabase
         })
     })
 })
 
+// Show the form to create a new user
 router.get('/new', (req, res)=>{
+    // render the new.ejs screen
     res.render('users/new.ejs');
 })
 
+// Get a user by ID
 router.get('/:id', (req, res)=>{
+    //  Find the User by ID
     User.findById(req.params.id) 
+    //  Load cats 
     .populate('cats')
+    //  search for a match in the user DB
     .exec((err, userFromTheDatabase)=>{
+        // check for an error
         if(err){
+            // send the error
             res.send(err);
+        // if there's no error
         } else {
+            // render the users screen
             res.render('users/show.ejs', {
-                userOnTheTemplate: userFromTheDatabase
-            });
+                // move the users from the DB to the ejs
+                userOnTheTemplate: userFromTheDatabase});
         }
 
     })
 })
 
+//  Get an existing user by ID
 router.get('/:id/edit', (req, res)=>{
+    //  Passes the User ID to the findById function
     User.findById(req.params.id, (err, userFromTheDatabase)=>{
+        // Render the edit.ejs
         res.render('users/edit.ejs', {
+            // move a user from the DB to the Template
             userOnTheTemplate: userFromTheDatabase
         })
     })
 })
 
+//  post the created user
 router.post('/', (req, res)=>{
+    // pass the body of the new form to the create function
     User.create(req.body, (err, newlyCreatedUser)=>{
+        // log the newly created user
         console.log(newlyCreatedUser)
+        // redirect back to the users page
         res.redirect('/users')
     })
 })
 
+//  get a user to be updated
 router.put('/:id', (req, res)=>{
+    //  pass the user ID to the function for update 
     User.findByIdAndUpdate(req.params.id, req.body, (err, userFromTheDatabase)=>{
+        // show the user to update
         console.log(userFromTheDatabase);
+        // redirect back to the Users page
         res.redirect('/users');
     })
 })
 
+// This function deletes a user.
 router.delete('/:id', (req, res)=>{
+    // Delete the user from the DB
     User.findByIdAndDelete(req.params.id, (err, userFromTheDatabase)=>{
+        // Show the deleted user
         console.log(userFromTheDatabase);
+        // Delete ALL the cats associated with the deleted user
         Cat.deleteMany({
             _id: {
+                // Mongo delete cats where the ID matches the User
                 $in: userFromTheDatabase.cats
             }
+            // Check for errors
         }, (err, data)=>{
+            // Show the error
             console.log(data);
+            // redirect back to the Users screen
             res.redirect('/users');
         })
     })
